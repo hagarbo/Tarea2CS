5f84f260d3a70daf2eae7103e399530de305aec9
hagarbo <hagarbo@gmail.com>
2023-11-02 21:57:36 +0100

    entrega realizada

--- fileManager.php
+++ fileManager.php
@@ -39,38 +39,32 @@ function showFilesInfo(array $info): string
 }
 
 const UPLOAD_ERRORS = array(
-    0=>"No hay error, fichero subido con éxito.",
-    1=>"El fichero subido excede la directiva upload_max_filesize de php.ini.",
-    2=>"El fichero subido excede la directiva MAX_FILE_SIZE especificada en el formulario HTML.",
-    3=>"El fichero fue sólo parcialmente subido.",
-    4=>"No se subió ningún fichero.",
-    6=>"Falta la carpeta temporal.",
-    7=>"No se pudo escribir el fichero en el disco.",
-    8=>"Una extensión de PHP detuvo la subida de ficheros."
+    0 => "No hay error, fichero subido con éxito.",
+    1 => "El fichero subido excede la directiva upload_max_filesize de php.ini.",
+    2 => "El fichero subido excede la directiva MAX_FILE_SIZE especificada en el formulario HTML.",
+    3 => "El fichero fue sólo parcialmente subido.",
+    4 => "No se subió ningún fichero.",
+    6 => "Falta la carpeta temporal.",
+    7 => "No se pudo escribir el fichero en el disco.",
+    8 => "Una extensión de PHP detuvo la subida de ficheros."
 );
-function uploadFiles(array $info){
-    for ($i=0; $i < count($info["name"]); $i++) {
+function uploadFiles(array $info)
+{
+    for ($i = 0; $i < count($info["name"]); $i++) {
         $file_name = $info["name"][$i];
         $tmp_file_name = $info["tmp_name"][$i];
         $error_code = $info["error"][$i];
         $file_size = $info["size"][$i];
-        
-        $upload_path = TARGET_FOLDER.DIRECTORY_SEPARATOR.$file_name;
-        if ($error_code==UPLOAD_ERR_OK && move_uploaded_file($tmp_file_name,$upload_path))
-        {
+
+        $upload_path = TARGET_FOLDER . DIRECTORY_SEPARATOR . $file_name;
+        if ($error_code == UPLOAD_ERR_OK && move_uploaded_file($tmp_file_name, $upload_path)) {
             echo "<details><summary class='msg-ok'>Se ha guardado con éxito el fichero: $file_name</summary>
-            <p><strong>Código de error: $error_code --> ".UPLOAD_ERRORS[$error_code]."</strong></p>"
-            ."<p><strong>Tamaño: $file_size Bytes</strong></p></details>";
-
-        }
-        else{
+            <p><strong>Código de error: $error_code --> " . UPLOAD_ERRORS[$error_code] . "</strong></p>"
+                . "<p><strong>Tamaño: $file_size Bytes</strong></p></details>";
+        } else {
             echo "<details><summary class='msg-err'>Error en la subida del fichero: $file_name</summary>
-            <p><strong>Código de error: $error_code --> ".UPLOAD_ERRORS[$error_code]."</strong></p>"
-            ."<p><strong>Tamaño: $file_size Bytes</strong></p></details>";
+            <p><strong>Código de error: $error_code --> " . UPLOAD_ERRORS[$error_code] . "</strong></p>"
+                . "<p><strong>Tamaño: $file_size Bytes</strong></p></details>";
         }
-    } 
+    }
 }
-
-
-
-
--- formulario.php
+++ formulario.php
@@ -1,6 +1,6 @@
 <?php
- require_once __DIR__.DIRECTORY_SEPARATOR."fileManager.php";
- const TARGET_FOLDER = __DIR__.DIRECTORY_SEPARATOR."img";
+require_once __DIR__ . DIRECTORY_SEPARATOR . "fileManager.php";
+const TARGET_FOLDER = __DIR__ . DIRECTORY_SEPARATOR . "img";
 
 ?>
 
@@ -13,12 +13,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario</title>
     <style>
-        body{
+        body {
             font-family: Verdana, Geneva, Tahoma, sans-serif;
             margin-top: 2em;
             margin-left: 20%;
             margin-right: 20%;
         }
+
         #file-info {
             display: flex;
             flex-wrap: wrap;
@@ -26,16 +27,19 @@
             border-width: 2px 0 0 2px;
         }
 
-        .file-data{
+        .file-data {
             flex-grow: 1;
             width: 500px;
             padding: 0 2em 1em 2em;
             border: solid #4169e1;
-	        border-width: 0 2px 2px 0;
-        }        
-        h1,h2{
+            border-width: 0 2px 2px 0;
+        }
+
+        h1,
+        h2 {
             text-align: center;
         }
+
         h4 {
             font-size: 1.2em;
             font-weight: bold;
@@ -50,23 +54,29 @@
         li {
             margin-bottom: 0.5em;
         }
-        form{
+
+        form {
             border: 2px solid green;
             border-radius: 5px;
             padding: 2em;
         }
-        form input{
+
+        form input {
             font-size: 1.2em;
         }
-        #upload-info{
+
+        #upload-info {
             padding: 0.5em;
         }
-        .msg-ok{
+
+        .msg-ok {
             color: green;
         }
-        .msg-err{
+
+        .msg-err {
             color: red;
         }
+
         details {
             padding: 0.5em 0.5em 0;
         }
@@ -74,6 +84,7 @@
         details[open] {
             padding: 0.5em;
         }
+
         details[open] summary {
             border-bottom: 1px solid #aaa;
             margin-bottom: 0.5em;
@@ -84,14 +95,14 @@
 <body>
     <h1>Tarea 03.1 - Adrian Garcia Bouzas</h1>
     <form action="#" method="post" enctype="multipart/form-data">
-        
+
         <div>
             <p>
                 <label for="ficheros">Adjunte uno o varios ficheros (Solo se aceptan imágenes en formato JPG):</label>
             </p>
             <p>
                 <!-- MAX_FILE_SIZE=40M debe preceder al campo de entrada del fichero -->
-                <input type="hidden" name="MAX_FILE_SIZE" value="4000000" />
+                <input type="hidden" name="MAX_FILE_SIZE" value="40000000" />
                 <input type="file" name="ficheros[]" id="ficheros" multiple accept=".jpg">
             </p>
         </div>
@@ -100,22 +111,23 @@
 
     </form>
 
-    
-        <?php
-            if (isset($_FILES["ficheros"])) { 
-            
-            // Impresion de los datos de los ficheros
-            echo "<h2>Estos son los datos de los ficheros</h2>";
-            echo "<div id='file-info'>";
-            echo showFilesInfo($_FILES["ficheros"]);
-            echo "</div>";
 
-            // Informacion del estado de las subidas
-            echo "<div id='upload-info'>";
-            uploadFiles($_FILES["ficheros"]);   
-            echo "</div>";
-        }
-      ?>
-    
+    <?php
+    if (isset($_FILES["ficheros"])) {
+
+        // Impresion de los datos de los ficheros
+        echo "<h2>Estos son los datos de los ficheros</h2>";
+        echo "<div id='file-info'>";
+        echo showFilesInfo($_FILES["ficheros"]);
+        echo "</div>";
+
+        // Informacion del estado de las subidas
+        echo "<div id='upload-info'>";
+        uploadFiles($_FILES["ficheros"]);
+        echo "</div>";
+    }
+    ?>
+
 </body>
+
 </html>
