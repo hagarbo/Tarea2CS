a16e2669231bdead4c69eee601f513c03924f105
hagarbo <hagarbo@gmail.com>
2023-11-02 16:49:28 +0100

    all done faltan capturas

--- fileManager.php
+++ fileManager.php
@@ -38,6 +38,16 @@ function showFilesInfo(array $info): string
     return $filesInfoHtml;
 }
 
+const UPLOAD_ERRORS = array(
+    0=>"No hay error, fichero subido con éxito.",
+    1=>"El fichero subido excede la directiva upload_max_filesize de php.ini.",
+    2=>"El fichero subido excede la directiva MAX_FILE_SIZE especificada en el formulario HTML.",
+    3=>"El fichero fue sólo parcialmente subido.",
+    4=>"No se subió ningún fichero.",
+    6=>"Falta la carpeta temporal.",
+    7=>"No se pudo escribir el fichero en el disco.",
+    8=>"Una extensión de PHP detuvo la subida de ficheros."
+);
 function uploadFiles(array $info){
     for ($i=0; $i < count($info["name"]); $i++) {
         $file_name = $info["name"][$i];
@@ -45,21 +55,22 @@ function uploadFiles(array $info){
         $error_code = $info["error"][$i];
         $file_size = $info["size"][$i];
         
-        if ($error_code==UPLOAD_ERR_OK && is_uploaded_file($tmp_file_name))
+        $upload_path = TARGET_FOLDER.DIRECTORY_SEPARATOR.$file_name;
+        if ($error_code==UPLOAD_ERR_OK && move_uploaded_file($tmp_file_name,$upload_path))
         {
-            $upload_path = TARGET_FOLDER.DIRECTORY_SEPARATOR.$file_name;
-            if (move_uploaded_file($tmp_file_name,$upload_path))
-                echo "<p class='msg-ok'>Se ha guardado con éxito el fichero: $file_name
-                (<strong>Código de error->$error_code, Tamaño->$file_size</strong>)</p>";
-            else{
-                echo "<p class='msg-err'>Error en la subida del fichero: $file_name 
-                (<strong>Código de error->$error_code, Tamaño->$file_size</strong>)</p>";
-            }
+            echo "<details><summary class='msg-ok'>Se ha guardado con éxito el fichero: $file_name</summary>
+            <p><strong>Código de error: $error_code --> ".UPLOAD_ERRORS[$error_code]."</strong></p>"
+            ."<p><strong>Tamaño: $file_size Bytes</strong></p></details>";
+
         }
         else{
-            echo "<p class='msg-err'>Error en la subida del fichero: $file_name 
-                (<strong>Código de error->$error_code, Tamaño->$file_size</strong>)</p>";
+            echo "<details><summary class='msg-err'>Error en la subida del fichero: $file_name</summary>
+            <p><strong>Código de error: $error_code --> ".UPLOAD_ERRORS[$error_code]."</strong></p>"
+            ."<p><strong>Tamaño: $file_size Bytes</strong></p></details>";
         }
     } 
 }
+
+
+
 
